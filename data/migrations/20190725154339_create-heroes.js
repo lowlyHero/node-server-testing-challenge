135cd621bbb18cee6d96dd40fa69f0fd33feede2
lowlyHero <e.nunn1009@gmail.com>
2019-07-25 16:24:48 -0400

    seeded dummy data into db

--- data/migrations/20190725154339_create-heroes.js
+++ data/migrations/20190725154339_create-heroes.js
@@ -1,6 +1,9 @@
-
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('heroes', tbl => {
+    tbl.increments('id');
+    tbl.string('name', 128).unique();
+    tbl.text('superpower').notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
